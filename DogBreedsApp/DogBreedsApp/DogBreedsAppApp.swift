2a71e78bf4f220d56e24596cd1ec83bfcfe3296b
Cesar Lima <cesarcamposdelima@gmail.com>
2025-07-12 09:10:43 -0300

    Create project DogBreedsApp

---
+++ DogBreedsApp/DogBreedsApp/DogBreedsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsAppApp.swift
+//  DogBreedsApp
+//
+//  Created by MacPro on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
